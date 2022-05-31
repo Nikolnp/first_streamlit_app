caecbc6675ab53bf04f10a62aa4e1e50053e2d01
Nikoleta Petkova <32556133+Nikolnp@users.noreply.github.com>
2022-05-31 20:39:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parrents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blubery Oatmeal')
+streamlit.text('Kale spinah and rocket smoothie')
+streamlit.text('Hard-boiled free range egg')
