6e17b912b4cff050d5275b09ac6b89df9d001350
Nikoleta Petkova <32556133+Nikolnp@users.noreply.github.com>
2023-02-17 12:48:54 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ streamlit.title('Blog')
 
 # "with" notation
 with streamlit.sidebar:
-   streamlit.image("http://clipart-library.com/new_gallery/52-522940_clipart-drops-prismatic-transparent-background-brain-png.png")
+   streamlit.image("https://i.pinimg.com/originals/42/43/74/424374bd1876ded649b5d6ddba2c3ab5.png")
    if streamlit.button('Say hello'):
       streamlit.write('Why hello there')
    else:
