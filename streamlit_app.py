dea2549823a0a8e774718d4828419900aa29eb91
Nikoleta Petkova <32556133+Nikolnp@users.noreply.github.com>
2023-02-18 13:45:54 +0000

    #Matplot lib and Numpy test example
    test visualisation

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit 
 import pandas
 import requests
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 
@@ -94,5 +96,17 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 #output it the screen as a table
 #streamlit.dataframe(fruityvice_normalized)
+#Matplot lib and numpy test example 
+x = np.linspace(0,2*np.pi,100)
+y = np.sin(x)
+y2 =np.cos(x)
+
+plt.title("Sin and cos from 0 to 2*pi")
+plt.xlabel("input")
+plt.ylabel("output")
+plt.plot(x,y)
+plt.plot(x,y2)
+plt.show()
+
 
 
