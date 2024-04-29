9981bb24ea25a9789907bdabfb786422ab541e11
Nikoleta Petkova <32556133+Nikolnp@users.noreply.github.com>
2024-04-29 23:13:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,10 +91,21 @@ def main():
         streamlit.text('🍆 Grilled Eggplant with Tomato Sauce')
         
     col1, col2, col3 = streamlit.columns(3) 
-    
     #Header of Smoothie Maker
     streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
     #initialise the dataframe
+    my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+    my_fruit_list = my_fruit_list.set_index('Fruit')
+    
+    #Add multiselect
+    fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+    fruits_to_show = my_fruit_list.loc[fruits_selected]
+    
+    #display the dataframe
+    streamlit.dataframe(fruits_to_show)
+    
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
    
 if __name__ == "__main__":
     main()
