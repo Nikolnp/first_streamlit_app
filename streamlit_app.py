a3a277ac4c87f487457f741722f583bea5e87c1d
Nikoleta Petkova <32556133+Nikolnp@users.noreply.github.com>
2024-04-29 22:16:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,6 @@ import json
 #Page Title
 #streamlit.title('Blog')
 # Object notation
-import streamlit as st
-import requests
 
 def get_weather_data(city, api_key):
     """Fetch weather data from OpenWeatherMap API."""
@@ -32,7 +30,17 @@ def display_weather(data):
     st.write(f"**Humidity:** {data['main']['humidity']}%")
 
 def main():
-    st.title("Weather Forecast App")
+   
+    # "with" notation
+with streamlit.sidebar:
+   streamlit.markdown("<h3 style='text-align: center; color: grey;'>Blog Content</h3>", unsafe_allow_html=True)
+   streamlit.image("https://irelandtravelguides.com/wp-content/uploads/2020/06/gold-foil-tree-of-life-5262414_640.png")
+   streamlit.caption('_"One rarely falls in love without being as much attracted to what is interestingly wrong with someone as what is objectively healthy."― Alain de Botton_')
+   col1, col2, col3 = streamlit.columns(3)
+   col1.metric("Temperature", "70 °F", "1.2 °F")
+   col2.metric("Wind", "9 mph", "-8%")
+   col3.metric("Humidity", "86%", "4%")
+    st.title("Weather Forecast")
     city = st.text_input("Enter a city name", "London")
 
     api_key = "1a4fb3f2dc6ead2387e5fed61756ddb3"
@@ -43,19 +51,7 @@ def main():
             display_weather(weather_data)
         else:
             st.error("City not found!")
-
-if __name__ == "__main__":
-    main()
-
-# "with" notation
-with streamlit.sidebar:
-   streamlit.markdown("<h3 style='text-align: center; color: grey;'>Blog Content</h3>", unsafe_allow_html=True)
-   streamlit.image("https://irelandtravelguides.com/wp-content/uploads/2020/06/gold-foil-tree-of-life-5262414_640.png")
-   streamlit.caption('_"One rarely falls in love without being as much attracted to what is interestingly wrong with someone as what is objectively healthy."― Alain de Botton_')
-   col1, col2, col3 = streamlit.columns(3)
-   col1.metric("Temperature", "70 °F", "1.2 °F")
-   col2.metric("Wind", "9 mph", "-8%")
-   col3.metric("Humidity", "86%", "4%")
+ 
    #if streamlit.button('HEALTH'):
     #  streamlit.write('Redirect to Healthy page')
    #else:
@@ -125,4 +121,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 #--------------------------------------------------------------------------------------------
 
+
+
+
+
+if __name__ == "__main__":
+    main()
 
