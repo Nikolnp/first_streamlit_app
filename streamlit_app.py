2b311341945e77e5627a8da9a8f48c32602ed4a6
Nikoleta Petkova <32556133+Nikolnp@users.noreply.github.com>
2024-04-29 22:20:15 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,88 +31,88 @@ def display_weather(data):
 
 def main():
    with streamlit.sidebar:
-   streamlit.markdown("<h3 style='text-align: center; color: grey;'>Blog Content</h3>", unsafe_allow_html=True)
-   streamlit.image("https://irelandtravelguides.com/wp-content/uploads/2020/06/gold-foil-tree-of-life-5262414_640.png")
-   streamlit.caption('_"One rarely falls in love without being as much attracted to what is interestingly wrong with someone as what is objectively healthy."― Alain de Botton_')
-   col1, col2, col3 = streamlit.columns(3)
-   col1.metric("Temperature", "70 °F", "1.2 °F")
-   col2.metric("Wind", "9 mph", "-8%")
-   col3.metric("Humidity", "86%", "4%")
-    st.title("Weather Forecast")
-    city = st.text_input("Enter a city name", "London")
+       streamlit.markdown("<h3 style='text-align: center; color: grey;'>Blog Content</h3>", unsafe_allow_html=True)
+       streamlit.image("https://irelandtravelguides.com/wp-content/uploads/2020/06/gold-foil-tree-of-life-5262414_640.png")
+       streamlit.caption('_"One rarely falls in love without being as much attracted to what is interestingly wrong with someone as what is objectively healthy."― Alain de Botton_')
+       col1, col2, col3 = streamlit.columns(3)
+       col1.metric("Temperature", "70 °F", "1.2 °F")
+       col2.metric("Wind", "9 mph", "-8%")
+       col3.metric("Humidity", "86%", "4%")
+        st.title("Weather Forecast")
+        city = st.text_input("Enter a city name", "London")
+    
+        api_key = "1a4fb3f2dc6ead2387e5fed61756ddb3"
 
-    api_key = "1a4fb3f2dc6ead2387e5fed61756ddb3"
-
-    if st.button("Get Weather"):
-        weather_data = get_weather_data(city, api_key)
-        if weather_data.get("cod") != 404:
-            display_weather(weather_data)
-        else:
-            st.error("City not found!")
- 
-
-
-streamlit.markdown("<h1 style='text-align: center; color: grey;'>HEALTHY CAN BE TAISTY</h1>", unsafe_allow_html=True)
-
-streamlit.image("http://www.pngall.com/wp-content/uploads/2016/07/Meditation-Transparent.png" )
-col4, col5, col6 = streamlit.columns(3)
-with col4:
-   streamlit.header('Breakfast Menu')
-   streamlit.text('🥣 Omega 3 and Bluberry Oatmeal')
-   streamlit.text(' 🥗 Kale spinah and rocket smoothie')
-   streamlit.text('🐔 Hard-boiled free range egg')
-   streamlit.text( '🥑🍞 Avocado Toast')
-with col5:
-   streamlit.image("http://www.pngall.com/wp-content/uploads/5/Diet-PNG-Clipart.png")
-with col6:
-   streamlit.header('Snack Menu')
-   streamlit.text('Apple')
-   streamlit.text('Kiwi')
-   streamlit.text('or another favorite fruit')
-#def columns list
-col1, col2, col3, = streamlit.columns(3)
-
-with col1:
-   streamlit.image("https://cdn.pixabay.com/photo/2014/04/03/10/38/yoga-310940_960_720.png")
-with col2:
-   streamlit.header('Lunch Menu')
-   streamlit.text('🥣 Omega 3 and Bluberry Oatmeal')
-   streamlit.text(' 🥗 Kale spinah and rocket smoothie')
-   streamlit.text('🐔 Hard-boiled free range egg')
-   streamlit.text( '🥑🍞 Avocado Toast')
-with col3:
-    streamlit.image("https://cdn.pixabay.com/photo/2014/04/02/10/48/woman-304646_640.png")
-col7, col8, col9, = streamlit.columns(3)
-with col7:
-   streamlit.header('Snack Menu')
-   streamlit.text('Carrots')
-   streamlit.text('Humous')
-with col8:
-   streamlit.image("https://cdn3.iconfinder.com/data/icons/wrestler/755/muscle_bodybuilding_bodybuilder_bicep_tricep_healthy_fitness-512.png")
-with col9:
-   streamlit.header('Dinner Menu')
-   streamlit.text('🥣 Omega 3 and Bluberry Oatmeal')
-   streamlit.text(' 🥗 Kale spinah and rocket smoothie')
-   streamlit.text('🐔 Hard-boiled free range egg')
-   streamlit.text( '🥑🍞 Avocado Toast')
-col1, col2, col3, = streamlit.columns(3) 
+        if st.button("Get Weather"):
+            weather_data = get_weather_data(city, api_key)
+            if weather_data.get("cod") != 404:
+                display_weather(weather_data)
+            else:
+                st.error("City not found!")
+     
 
 
-#Header of Smootie Maker
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-#initialise the dataframe
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list =  my_fruit_list.set_index('Fruit')
-
-#Add multiselect
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-#display the dataframe
-streamlit.dataframe(fruits_to_show)
- 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.markdown("<h1 style='text-align: center; color: grey;'>HEALTHY CAN BE TAISTY</h1>", unsafe_allow_html=True)
+    
+    streamlit.image("http://www.pngall.com/wp-content/uploads/2016/07/Meditation-Transparent.png" )
+    col4, col5, col6 = streamlit.columns(3)
+    with col4:
+       streamlit.header('Breakfast Menu')
+       streamlit.text('🥣 Omega 3 and Bluberry Oatmeal')
+       streamlit.text(' 🥗 Kale spinah and rocket smoothie')
+       streamlit.text('🐔 Hard-boiled free range egg')
+       streamlit.text( '🥑🍞 Avocado Toast')
+    with col5:
+       streamlit.image("http://www.pngall.com/wp-content/uploads/5/Diet-PNG-Clipart.png")
+    with col6:
+       streamlit.header('Snack Menu')
+       streamlit.text('Apple')
+       streamlit.text('Kiwi')
+       streamlit.text('or another favorite fruit')
+    #def columns list
+    col1, col2, col3, = streamlit.columns(3)
+    
+    with col1:
+       streamlit.image("https://cdn.pixabay.com/photo/2014/04/03/10/38/yoga-310940_960_720.png")
+    with col2:
+       streamlit.header('Lunch Menu')
+       streamlit.text('🥣 Omega 3 and Bluberry Oatmeal')
+       streamlit.text(' 🥗 Kale spinah and rocket smoothie')
+       streamlit.text('🐔 Hard-boiled free range egg')
+       streamlit.text( '🥑🍞 Avocado Toast')
+    with col3:
+        streamlit.image("https://cdn.pixabay.com/photo/2014/04/02/10/48/woman-304646_640.png")
+    col7, col8, col9, = streamlit.columns(3)
+    with col7:
+       streamlit.header('Snack Menu')
+       streamlit.text('Carrots')
+       streamlit.text('Humous')
+    with col8:
+       streamlit.image("https://cdn3.iconfinder.com/data/icons/wrestler/755/muscle_bodybuilding_bodybuilder_bicep_tricep_healthy_fitness-512.png")
+    with col9:
+       streamlit.header('Dinner Menu')
+       streamlit.text('🥣 Omega 3 and Bluberry Oatmeal')
+       streamlit.text(' 🥗 Kale spinah and rocket smoothie')
+       streamlit.text('🐔 Hard-boiled free range egg')
+       streamlit.text( '🥑🍞 Avocado Toast')
+    col1, col2, col3, = streamlit.columns(3) 
+    
+    
+    #Header of Smootie Maker
+    streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+    #initialise the dataframe
+    my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+    my_fruit_list =  my_fruit_list.set_index('Fruit')
+    
+    #Add multiselect
+    fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+    fruits_to_show = my_fruit_list.loc[fruits_selected]
+    
+    #display the dataframe
+    streamlit.dataframe(fruits_to_show)
+     
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 
 
