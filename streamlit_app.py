b486be7bda0a6b4b150abbbcc88f8bbe3c428441
Nikolnp <32556133+Nikolnp@users.noreply.github.com>
2026-04-23 23:13:08 +0300

    Add comment for database path in streamlit_app.py
    Added a comment indicating the path to the database.

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ from PIL import Image
 from database import init_db, save_user_and_emissions, load_emissions
 init_db()
 st.write("DB exists:", os.path.exists("sustainability.db"))
+#path to database
 st.write(os.getcwd())
 
 def get_weather(city, api_key):
