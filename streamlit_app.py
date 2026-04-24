0646ea4b52e8363c67910e44ece45b4e6b865413
Nikolnp <32556133+Nikolnp@users.noreply.github.com>
2026-04-24 15:06:46 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -184,6 +184,23 @@ def main():
                         "Rain Probability",
                         f"{round(rain_prob * 100)}%"
                     )
+            st.title("Animated Dice Roll 🎲")
+
+            dice_faces = ["⚀","⚁","⚂","⚃","⚄","⚅"]
+        
+            if st.button("Roll Dice Animation"):
+                placeholder = st.empty()
+                # animation loop
+                for _ in range(12):
+                    roll = random.randint(1,6)
+                    placeholder.markdown(
+                        f"<h1 style='text-align:center'>{dice_faces[roll-1]}</h1>",
+                        unsafe_allow_html=True
+                    )
+                    time.sleep(0.07)
+            # final result
+            st.success(f"Final result: {roll}")
+
     st.markdown("<h1 style='text-align: center; color: grey;'>HEALTHY - WEALTHY</h1>", unsafe_allow_html=True)
     st.markdown("<h2 style='text-align: center; color: grey;'>Тhe true science behind good choices</h2>", unsafe_allow_html=True)
         
@@ -270,23 +287,7 @@ def main():
     st.subheader("2. Brahmari Pranayama (Bumble Bee Breath)")
     st.markdown("[Watch Brahmari Pranayama on EkhartYoga](https://www.ekhartyoga.com/classes/3863/brahmari-pranayama-bumble-bee-breath)")
 
-    st.title("Animated Dice Roll 🎲")
-
-    dice_faces = ["⚀","⚁","⚂","⚃","⚄","⚅"]
-
-    if st.button("Roll Dice Animation"):
-        placeholder = st.empty()
-        # animation loop
-        for _ in range(12):
-            roll = random.randint(1,6)
-            placeholder.markdown(
-                f"<h1 style='text-align:center'>{dice_faces[roll-1]}</h1>",
-                unsafe_allow_html=True
-            )
-            time.sleep(0.07)
-    # final result
-    st.success(f"Final result: {roll}")
-
+    
     # =========================================================
     # 🌍 HOUSEHOLD SUSTAINABILITY CALCULATOR (SQL + IQR READY)
     # =========================================================
