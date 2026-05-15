7b362dc518869131a52f454e5213ca07e2752390
Nikolnp <32556133+Nikolnp@users.noreply.github.com>
2026-05-15 13:31:39 +0000

    codespaces update

--- weather.py
+++ weather.py
@@ -143,7 +143,6 @@ def weather_section():
             st.write(
                 f"**Humidity:** {data['humidity']}%"
             )
-            #st.devider()
             # =========================================================
             # RAIN PROBABILITY
             # =========================================================
@@ -155,7 +154,6 @@ def weather_section():
                     "Rain Probability",
                     f"{round(rain_prob * 100)}%"
                 )
-            #st.devider()
             # =========================================================
             # WEATHER DECISION DICE
             # =========================================================
@@ -199,7 +197,6 @@ def weather_section():
                     st.success(
                         "☔ Though it might rain, to take an umbrella or not is your decision"
                     )
-                #st.devider() 
         except Exception as e:
             st.error(
                 f"Unexpected display error: {e}"
